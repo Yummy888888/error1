2d0f69252d644eec6c69ec79d06a61d551c6f63c
Yummy888888 <164147616+Yummy888888@users.noreply.github.com>
2024-07-17 14:42:46 +0800

    Update urls.py

--- mblog/urls.py
+++ mblog/urls.py
@@ -14,12 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
-from mysite.views import homepage, post_detail
+from .views import PostDetailView
 
 urlpatterns = [
-    path("",homepage, name="home"),
-    path("admin/", admin.site.urls),
-    path("post/<slug:slug>/", post_detail, name="post_detail"),
+    path('post/<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
+    # 其他路由設置
 ]
