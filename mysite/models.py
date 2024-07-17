18825c877622b2c7001b4080cfd3fbf809ade880
Yummy888888 <164147616+Yummy888888@users.noreply.github.com>
2024-07-17 14:30:51 +0800

    Update models.py

--- mysite/models.py
+++ mysite/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils.text import slugify
 class Post(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)  # 使用 SlugField 來自動生成網址友好的 slug
@@ -11,3 +11,8 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+        
+    def save(self, *args, **kwargs):
+        # 在保存前自動生成slug
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
