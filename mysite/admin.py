6bbee2c567c42c1565109fdc40e84810f3e84739
Yummy888888 <164147616+Yummy888888@users.noreply.github.com>
2024-07-17 14:58:51 +0800

    Update admin.py

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from mysite.models import Post
-class Postadmin(admin.ModelAdmin):
-  list_display=("title","slug","pub_date")
-admin.site.register(Post,Postadmin)
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'pub_date')  # 確保這些字段在 Post 模型中是存在的
+
+admin.site.register(Post, PostAdmin)
