7f2711d2a2f4e18ecc9b852a9d22a5211da36a05
Yummy888888 <164147616+Yummy888888@users.noreply.github.com>
2024-07-17 15:01:44 +0800

    Update models.py

--- mysite/models.py
+++ mysite/models.py
@@ -4,7 +4,12 @@ from django.utils.text import slugify
 class Post(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)  # 假設這裡是你的 SlugField
-
+    pub_date = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+    return self.title
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)  # 使用標題來生成 slug
         super().save(*args, **kwargs)
+
+
+
