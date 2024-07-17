1e7a13339bf8439dd0dc167b09c65dfb214b7d5f
Yummy888888 <164147616+Yummy888888@users.noreply.github.com>
2024-07-17 16:22:39 +0800

    Update views.py

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import get_object_or_404, render
 from mysite.models import Post
+from datetime import datetime
 
 def post_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
     return render(request, 'post_detail.html', {'post': post})
+
+def homepage(request):
+    posts = Post.objects.all()
+    now = datetime.now()
+    return render(request, "index.html", {'posts': posts, 'now': now})
