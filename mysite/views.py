ea7d2e1b2805b5ffb5f8519bf664f183055f1ff6
Yummy888888 <164147616+Yummy888888@users.noreply.github.com>
2024-07-17 14:10:56 +0800

    Update views.py

--- mysite/views.py
+++ mysite/views.py
@@ -5,3 +5,7 @@ def homepage(request):
   posts=Post.objects.all()
   now=datetime.now()
   return render(request,"index.html",locals())
+def post_detail(request, slug):
+    # 實現詳細視圖的邏輯，這裡示範了一個簡單的方式
+    post = Post.objects.get(slug=slug)
+    return render(request, "post_detail.html", {'post': post})
