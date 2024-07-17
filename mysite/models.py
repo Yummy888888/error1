f2c3284b595c02a22678699a4d625998f562b6f5
Yummy888888 <164147616+Yummy888888@users.noreply.github.com>
2024-07-17 16:39:24 +0800

    Update models.py

--- mysite/models.py
+++ mysite/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 from django.utils.text import slugify
+import urllib.parse
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)  # 假設這裡是你的 SlugField
+    slug = models.SlugField(unique=True)
     pub_date = models.DateTimeField(auto_now_add=True)
+    original_url = models.URLField(max_length=200)  # 添加一个字段用来存储原始 URL
 
     def __str__(self):
         return self.title
 
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)  # 使用標題來生成 slug
+        self.slug = slugify(self.title)  # 使用标题生成 slug
+        self.original_url = urllib.parse.quote(self.original_url)  # 编码原始 URL
         super().save(*args, **kwargs)
