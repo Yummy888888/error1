07a2d90ce9804f3e2e188f1cd4a937feb989ddb5
Yummy888888 <164147616+Yummy888888@users.noreply.github.com>
2024-07-17 14:18:28 +0800

    Update models.py

--- mysite/models.py
+++ mysite/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 
-# Create your models here.
 class Post(models.Model):
-  title=models.CharField(max_length=200)
-  slug=models.CharField(max_length=200)
-  body=models.TextField()
-  pub_date=models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)  # 使用 SlugField 來自動生成網址友好的 slug
+    body = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
 
-class Meta:
-  ordering=("-pub_date",)
+    class Meta:
+        ordering = ("-pub_date",)
 
-def __str__(self):
-  return self.title 
+    def __str__(self):
+        return self.title
