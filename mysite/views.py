40a7bebc736639ed8a5176f56a4b0a8b054426cb
Yummy888888 <164147616+Yummy888888@users.noreply.github.com>
2024-07-17 15:07:57 +0800

    Update views.py

--- mysite/views.py
+++ mysite/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import DetailView
 from mysite.models import Post
+from django.shortcuts import render
+from datetime import datetime
 
 class PostDetailView(DetailView):
     model = Post
@@ -8,3 +10,9 @@ class PostDetailView(DetailView):
     queryset = Post.objects.all()  # 這裡可以自定義需要的 queryset 條件
     slug_field = 'slug'  # 如果使用 slug 來查找，確保與模型中的字段名稱一致
     slug_url_kwarg = 'slug'  # URLconf 中的參數名稱
+
+def homepage(request):
+    posts = Post.objects.all()
+    now = datetime.now()
+    return render(request, "index.html", {'posts': posts, 'now': now})
+
