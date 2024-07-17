8d004cb21fe910fe2f6db9610e291e7ff4ae482a
Yummy888888 <164147616+Yummy888888@users.noreply.github.com>
2024-07-17 14:55:47 +0800

    Update views.py

--- mysite/views.py
+++ mysite/views.py
@@ -1,19 +1,10 @@
-from django.shortcuts import render
-from datetime import datetime
+from django.views.generic import DetailView
 from mysite.models import Post
 
-def homepage(request):
-    posts = Post.objects.all()
-    now = datetime.now()
-    return render(request, "index.html", {'posts': posts, 'now': now})
-
-def post_detail(request, slug):
-    # 假設你的 Post 模型有 slug 字段，根據 slug 查詢相應的文章
-    try:
-        post = Post.objects.get(slug=slug)
-    except Post.DoesNotExist:
-        # 如果文章不存在，可以返回一個 404 錯誤頁面或其他處理方式
-        from django.http import Http404
-        raise Http404("Post does not exist")
-
-    return render(request, "post_detail.html", {'post': post})
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+    context_object_name = 'post'
+    queryset = Post.objects.all()  # 這裡可以自定義需要的 queryset 條件
+    slug_field = 'slug'  # 如果使用 slug 來查找，確保與模型中的字段名稱一致
+    slug_url_kwarg = 'slug'  # URLconf 中的參數名稱
